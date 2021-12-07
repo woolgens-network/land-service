2b9d855bdc168a9bd62409b1068ac8a795ab0049
ReaperMaga <reapermaga@gmail.com>
2021-12-07 02:01:00 +0100

    Initial commit

---
+++ src/main/java/net/woolgens/scaffold/auth/AuthWrapper.java
@@ -0,0 +1,33 @@
+package net.woolgens.scaffold.auth;
+
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.security.spi.runtime.AuthorizationController;
+import lombok.Getter;
+import lombok.Setter;
+import net.woolgens.library.auth.AuthBootstrap;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+/**
+ * Copyright (c) Maga, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Maga
+ **/
+@ApplicationScoped
+@Getter
+public class AuthWrapper {
+
+    AuthBootstrap bootstrap;
+
+
+    public void onStart(@Observes StartupEvent event) {
+        try {
+            this.bootstrap = new AuthBootstrap("config/");
+        }catch (Exception exception){
+            System.out.println("There was an error while trying to initialize auth bootstrap");
+        }
+    }
+
+}
